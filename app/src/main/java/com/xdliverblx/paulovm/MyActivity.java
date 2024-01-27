b46fb3048d9de96fbc4521e7665b7a0d9dbe898c
xdlivecat <xdliverblx@hotmail.com>
2024-01-27 15:37:42 +0100

    Begun work on new UI, removed ads

--- app/src/main/java/com/xdliverblx/paulovm/MyActivity.java
+++ app/src/main/java/com/xdliverblx/paulovm/MyActivity.java
@@ -35,37 +35,25 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.PopupMenu;
 import android.widget.Toast;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.RequestConfiguration;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import com.xdliverblx.paulovm.R;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyActivity extends AppCompatActivity {
 
-
-    private static final String AD_UNIT_ID = "ca-app-pub-1451772312405789/2616836736";
-    private static final String TAG = "MyActivity";
-    private final AtomicBoolean isMobileAdsInitializeCalled = new AtomicBoolean(false);
-    private GoogleMobileAdsConsentManager googleMobileAdsConsentManager;
-    private AdView adView;
-    private FrameLayout adContainerView;
-    private AtomicBoolean initialLayoutComplete = new AtomicBoolean(false);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        WebView webView=(WebView) findViewById(R.id.webview);
+        WebView webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://paulovm.com/");
         webView.getSettings().setDomStorageEnabled(true);
@@ -78,9 +66,9 @@ public class MyActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.setClickable(true);
         webView.setWebChromeClient(new WebChromeClient());
-        WebSettings webSettings=webView.getSettings();
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        adContainerView = findViewById(R.id.ad_view_container);
+
 
 
         Button toggleKeyboardButton = findViewById(R.id.toggleKeyboardButton);
@@ -100,169 +88,6 @@ public class MyActivity extends AppCompatActivity {
                 webView.clearCache(true);
             }
         });
-
-
-        Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion());
-
-        googleMobileAdsConsentManager =
-                GoogleMobileAdsConsentManager.getInstance(getApplicationContext());
-        googleMobileAdsConsentManager.gatherConsent(
-                this,
-                consentError -> {
-                    if (consentError != null) {
-                        // Consent not obtained in current session.
-                        Log.w(
-                                TAG,
-                                String.format("%s: %s", consentError.getErrorCode(), consentError.getMessage()));
-                    }
-
-                    if (googleMobileAdsConsentManager.canRequestAds()) {
-                        initializeMobileAdsSdk();
-                    }
-
-                    if (googleMobileAdsConsentManager.isPrivacyOptionsRequired()) {
-                        // Regenerate the options menu to include a privacy setting.
-                        invalidateOptionsMenu();
-                    }
-                });
-
-        // This sample attempts to load ads using consent obtained in the previous session.
-        if (googleMobileAdsConsentManager.canRequestAds()) {
-            initializeMobileAdsSdk();
-        }
-
-        // Since we're loading the banner based on the adContainerView size, we need to wait until this
-        // view is laid out before we can get the width.
-        adContainerView
-                .getViewTreeObserver()
-                .addOnGlobalLayoutListener(
-                        () -> {
-                            if (!initialLayoutComplete.getAndSet(true)
-                                    && googleMobileAdsConsentManager.canRequestAds()) {
-                                loadBanner();
-                            }
-                        });
-
-        // Set your test devices. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-        // to get test ads on this device."
-        MobileAds.setRequestConfiguration(
-                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345")).build());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action_menu, menu);
-        MenuItem moreMenu = menu.findItem(R.id.action_more);
-        moreMenu.setVisible(googleMobileAdsConsentManager.isPrivacyOptionsRequired());
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        View menuItemView = findViewById(item.getItemId());
-        PopupMenu popup = new PopupMenu(this, menuItemView);
-        popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
-        popup.show();
-        popup.setOnMenuItemClickListener(
-                popupMenuItem -> {
-                    if (popupMenuItem.getItemId() == R.id.privacy_settings) {
-                        // Handle changes to user consent.
-                        googleMobileAdsConsentManager.showPrivacyOptionsForm(
-                                this,
-                                formError -> {
-                                    if (formError != null) {
-                                        Toast.makeText(this, formError.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                        return true;
-                    }
-                    return false;
-                });
-        return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when leaving the activity */
-    @Override
-    public void onPause() {
-        if (adView != null) {
-            adView.pause();
-        }
-        super.onPause();
-    }
-
-    /** Called when returning to the activity */
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) {
-            adView.resume();
-        }
-    }
-
-    /** Called before the activity is destroyed */
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
-
-
-    private void loadBanner() {
-        // Create a new ad view.
-        adView = new AdView(this);
-        adView.setAdUnitId(AD_UNIT_ID);
-        adView.setAdSize(getAdSize());
-
-        // Replace ad container with new ad view.
-        adContainerView.removeAllViews();
-        adContainerView.addView(adView);
-
-        // Start loading the ad in the background.
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-    }
-
-
-    private void initializeMobileAdsSdk() {
-        if (isMobileAdsInitializeCalled.getAndSet(true)) {
-            return;
-        }
-
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(
-                this,
-                new OnInitializationCompleteListener() {
-                    @Override
-                    public void onInitializationComplete(InitializationStatus initializationStatus) {}
-                });
-
-        // Load an ad.
-        if (initialLayoutComplete.get()) {
-            loadBanner();
-        }
-    }
-
-    private AdSize getAdSize() {
-        // Determine the screen width (less decorations) to use for the ad width.
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        display.getMetrics(outMetrics);
-
-        float density = outMetrics.density;
-
-        float adWidthPixels = adContainerView.getWidth();
-
-        // If the ad hasn't been laid out, default to the full screen width.
-        if (adWidthPixels == 0) {
-            adWidthPixels = outMetrics.widthPixels;
-        }
-
-        int adWidth = (int) (adWidthPixels / density);
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
     }
     public class mywebClient extends WebViewClient {
         @Override
@@ -291,6 +116,30 @@ public class MyActivity extends AppCompatActivity {
             // If the keyboard is currently visible, hide it. If not visible, show it.
             imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
         }
+    }
+    public void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        // Handle Settings click
+                        return true;
+                    case R.id.action_help:
+                        // Handle Help click
+                        return true;
+                    case R.id.action_about:
+                        // Handle About click
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
     }
 
 }
